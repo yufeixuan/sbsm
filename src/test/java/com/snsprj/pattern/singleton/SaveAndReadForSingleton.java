8f6d8d72586d01d86439b4c4b005f188357bbd5a
syoukaihou@gmail.com <syoukaihou@gmail.com>
2019-02-18 20:08:19 +0800

    单例模式--枚举实现

--- src/test/java/com/snsprj/pattern/singleton/SaveAndReadForSingleton.java
+++ src/test/java/com/snsprj/pattern/singleton/SaveAndReadForSingleton.java
@@ -1,13 +1,17 @@
 package com.snsprj.pattern.singleton;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 序列化反序列化单例
  */
+@Slf4j
 public class SaveAndReadForSingleton {
 
     public static void main(String[] args) {
@@ -20,12 +24,31 @@ public class SaveAndReadForSingleton {
             FileOutputStream fileOutputStream = new FileOutputStream(file);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
 
-            objectOutputStream.write(singleton);
+            objectOutputStream.writeObject(singleton);
 
+            fileOutputStream.close();
+            objectOutputStream.close();
 
+            log.info("====>序列化前：{}",singleton.hashCode());
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+            InnerClassSingleton rSingleton = (InnerClassSingleton) objectInputStream.readObject();
+
+            fileInputStream.close();
+            objectInputStream.close();
+
+            log.info("====>序列化后：{}", rSingleton.hashCode());
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
